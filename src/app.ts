3cac9c69e5569d4dd112a4e0ad56635d9c2b821d
Thales <thales.souz@outlook.com>
2022-10-06 14:57:26 -0300

    build: rotas funcionando

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import { ProductsRoutes } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', ProductsRoutes);
+
+app.get('/ping', (req, res) => {
+  res.status(200).json('pong');
+});
 
 export default app;
